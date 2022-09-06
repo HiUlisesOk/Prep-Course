6f21f3a0b6673000521d500ae40dbe58d8581148
Ulises Esquivel <uesquivel95@gmail.com>
2022-09-05 22:22:31 -0300

    tarea terminada

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -154,6 +154,9 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  if (num1 === 0 || num2 === 0 || num3 === 0){
+    return "Error";
+}
   if (num1 < 0 || num2 < 0 || num3 <0){
     return "Hay negativos";
 }
@@ -221,9 +224,14 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aqui
-  var digitos = Array.from(arguments);
-for (var i = 0; i < 10; i++){
-  console.log(digitos[0]);
+  var digitos = numero;
+  var digitos = "" + digitos + ""; 
+for (var i = 0; i < digitos.length; i++){
+  if (digitos.length === 3){
+    return true;
+  }else {
+    return false;
+  }
 }
 
 }
@@ -233,6 +241,15 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var i = 0;
+  do {
+    var numero = numero + 5;
+    console.log( numero + ' ' + i )
+    	i++;
+    if (i === 8){
+      return numero + 5;
+    }
+  } while(i != 8);
 }
 
 
