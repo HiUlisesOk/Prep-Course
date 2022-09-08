416da4e07c3bb8e29acfc31c791aa91d2000f203
Ulises Esquivel <uesquivel95@gmail.com>
2022-09-08 18:51:33 -0300

    otra message

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -90,7 +90,7 @@ if (numeros[i] === Math.floor(numeros[i])){
 }
 
 
-function promedioResultadoTest(resultadosTest) {
+function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
